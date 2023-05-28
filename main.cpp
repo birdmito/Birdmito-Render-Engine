88bdfc118822c8b52b7166face941fab6b8361fc
Birdmito <bird15600660082@126.com>
2023-05-28 20:39:08 +0800

    update

--- main.cpp
+++ main.cpp
@@ -99,6 +99,8 @@ int main()
     // configure global opengl state
     // -----------------------------
     glEnable(GL_DEPTH_TEST);
+    // tell stb_image.h to flip loaded texture's on the y-axis (before loading model).
+    stbi_set_flip_vertically_on_load(true);
     
 #endif //INITIALIZATION
 
